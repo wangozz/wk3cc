a10ed9d6c98d97e756a427c41ddfd5aa1ce436cb
SamuelWango <wangos478@gmail.com>
2023-09-11 00:38:37 +0300

    fin

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# wk3cc/main.py
 
 from restaurant.restaurant import Restaurant
 from customer.customer import Customer
@@ -6,34 +5,26 @@ from review.review import Review
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Set up the database connection
 engine = create_engine('sqlite:///database.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Example usage:
 
-# Creating a new restaurant
 new_restaurant = Restaurant(name='New Restaurant', price=3)
 session.add(new_restaurant)
 
-# Creating a new customer
 new_customer = Customer(first_name='John', last_name='Doe')
 session.add(new_customer)
 
-# Adding a review
 new_review = Review(star_rating=4, restaurant=new_restaurant, customer=new_customer)
 session.add(new_review)
 
-# Committing changes to the database
 session.commit()
 
-# Fetching data
 restaurants = session.query(Restaurant).all()
 customers = session.query(Customer).all()
 reviews = session.query(Review).all()
 
-# Printing data
 print("Restaurants:")
 for restaurant in restaurants:
     print(f"ID: {restaurant.id}, Name: {restaurant.name}, Price: {restaurant.price}")
